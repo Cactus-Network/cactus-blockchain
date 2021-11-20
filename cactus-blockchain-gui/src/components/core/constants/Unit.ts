2278d66ff4ac0e04a4fb2c253d2bbb7db90218f7
Dominick <sgroiwes@gmail.com>
2021-11-20 08:18:51 -0700

    submodule gui

--- cactus-blockchain-gui/src/components/core/constants/Unit.ts
+++
@@ -1,7 +0,0 @@
-enum Unit {
-  CACTUS = 'CACTUS',
-  MOJO = 'MOJO',
-  COLOURED_COIN = 'COLOUREDCOIN',
-}
-
-export default Unit;
--- cactus-blockchain-gui/src/constants/PlotterName.ts
+++
@@ -1,7 +0,0 @@
-enum PlotterName {
-  BLADEBIT = 'bladebit',
-  CACTUSPOS = 'cactuspos',
-  MADMAX = 'madmax',
-}
-
-export default PlotterName;
