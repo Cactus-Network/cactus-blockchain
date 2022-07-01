0b48d68d5ed1d96f16e1c5690b86d35206f50ef9
sgroiwes <sgroiwes@gmail.com>
2022-07-01 13:22:42 -0700

    update 1.4

--- installhelper.py
+++ installhelper.py
@@ -44,7 +44,7 @@ def make_semver(version_str: str) -> str:
     if local:
         version += "+{0}".format(local)
 
-    return "1.4"
+    return "1.4" #Dommer
 
 
 def get_cactus_version() -> str:
